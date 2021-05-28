0d782673c67bfa805de56cb37abec7767592bc74
второй <646sanek@rambler.ru>
2021-05-28 11:17:39 +0400

    print

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
         int[] array = new int[10];
         for (int i = 0; i < 10; i++) {
             array[i] = i + 1;
-            System.out.println("число" + array[i]);
+            System.out.println("number" +
+                    "" + array[i]);
+            System.out.println("ИИИИИ....");
         }
     }
 
