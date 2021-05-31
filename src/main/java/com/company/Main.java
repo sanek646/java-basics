5f5fe9cbc574b3646a0734f644f9a14f6c2b3397
второй <646sanek@rambler.ru>
2021-05-31 14:47:16 +0400

    проверь 1

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] num = new int[]{1,2,3,4,4,4,6,6,5,8,3,2};
+        int[] num = {1,2,3,4,4,4,6,6,5,8,3,2};
         for (int i=0; i<(num.length); i++ ) {
             System.out.println(num[i]);
         }
