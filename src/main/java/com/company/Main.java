9e528b2b9132a6c60068ebbf916f3ac2ba841ca2
второй <646sanek@rambler.ru>
2021-05-31 12:22:56 +0400

    не понял вопроса

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,15 +1,28 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[] array = new int[10];
-        for (int i = 0; i < 10; i++) {
-            array[i] = i + 1;
-            System.out.println("number" +
-                    "" + array[i]);
-            System.out.println("CH....");
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter number:");
+        String strInput = input.readLine();
+        int size = Integer.parseInt(strInput);
+
+        System.out.println("array length" + size);
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter" + (i + 1) + "array element");
+            String strArr = input.readLine();
+            array[i] = Integer.parseInt(strArr);
+        }
+        System.out.println("Output of the entire array by elements");
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + "array element" + array[i]);
         }
     }
-
 }
+
