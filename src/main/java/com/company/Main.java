9538214eef7c3aafa7eb6033762d9052ee7ba166
второй <646sanek@rambler.ru>
2021-05-31 15:50:35 +0400

    проверь 2

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] num = {1,2,3,4,4,4,6,6,5,8,3,2};
-        for (int i=0; i<(num.length); i++ ) {
+        for (int i : num ) {
             System.out.println(num[i]);
         }
 
