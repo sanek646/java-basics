333fcc770303d34cb2c6d00bdfb9537cacd3f206
второй <646sanek@rambler.ru>
2021-05-31 14:39:49 +0400

    проверь

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,28 +1,13 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter number:");
-        String strInput = input.readLine();
-        int size = Integer.parseInt(strInput);
+    public static void main(String[] args) {
+        int[] num = new int[]{1,2,3,4,4,4,6,6,5,8,3,2};
+        for (int i=0; i<(num.length); i++ ) {
+            System.out.println(num[i]);
+        }
 
-        System.out.println("array length:" + size);
-        int[] array = new int[size];
-        for (int i = 0; i < size; i++) {
-            System.out.println("Enter:" + (i + 1) + "array element:");
-            String strArr = input.readLine();
-            array[i] = Integer.parseInt(strArr);
-        }
-        System.out.println("Output of the entire array by elements:");
-        for (int i = 0; i < size; i++) {
-            System.out.println((i + 1) + "array element:" + array[i]);
-        }
     }
 }
 
