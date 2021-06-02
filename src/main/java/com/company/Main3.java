5e5c6b16f1537a5af0d625696da2348cfd7b0dd3
второй <646sanek@rambler.ru>
2021-06-02 08:17:28 +0400

    start

---
+++ src/main/java/com/company/Main3.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Hashtable;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Hashtable<String, String> map = new Hashtable<>();
+        map.put("A", "Oleg");
+        map.put("B", "Sasha");
+        map.put("C", "Andrey");
+        map.put("D", "Kolya");
+        map.put("E", "Vasya");
+        map.put("F", "Petya");
+        map.put("G", "Andrey");
+        map.put("H", "Oleg");
+        map.put("I", "Sasha");
+        map.put("J", "Andrey");
+
+    }
+}
