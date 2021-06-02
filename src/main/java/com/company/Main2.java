35073253c2632174b848bd6905a25df7abada394
второй <646sanek@rambler.ru>
2021-06-02 09:08:01 +0400

    array cherez bufer

--- src/main/java/com/company/Main2.java
+++ src/main/java/com/company/Main2.java
@@ -1,11 +1,27 @@
 package com.company;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
 
 public class Main2 {
-    public static void main(String arg[]) {
-        //here is second task solution
+    public static void main(String[] args) throws IOException {
+        Reader r = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(r);
+
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 5; i++)
+        {
+            String s = reader.readLine();
+            list.add(s);
+        }
+        System.out.println("list" + list);
+
+
     }
 }
 
