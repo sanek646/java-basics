335f607d53feb656d789378b8088ffce56f28fed
второй <646sanek@rambler.ru>
2021-06-03 14:10:54 +0400

    tuta2

--- src/main/java/com/company/Main4.java
+++ src/main/java/com/company/Main4.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Iterator;
+
 
 public class Main4 {
     public static void main(String[] args) throws IOException {
