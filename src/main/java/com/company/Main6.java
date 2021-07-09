a9b6ed62fd1903ce531befc9e798b94d4335237a
второй <646sanek@rambler.ru>
2021-07-09 12:50:07 +0400

    table SQL

--- src/main/java/com/company/Main6.java
+++ src/main/java/com/company/Main6.java
@@ -4,11 +4,11 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Main6 {
-    static final String url = "jdbc:postgresql://127.0.0.1:5432/*******";
-    static final String user = "sanek";
-    static final String pass = "sanek";
-    static final String bd = "Id, fistName, lastName, companiId, role";
-
+    /* static final String url = "jdbc:postgresql://127.0.0.1:5432/new";
+     static final String user = "sanek";
+     static final String pass = "sanek";
+     String bd = "Id, fistName, lastName, companyId, role";
+ */
     public static void main(String[] args) {
         ArrayList<User> users = new ArrayList<>();
 
@@ -17,31 +17,29 @@ public class Main6 {
         users.add(new User(3244, "bob", "c", 347913, "ing"));
         users.add(new User(3257, "den", "d", 347913, "ing"));
         users.add(new User(3266, "alex", "g", 347913, "sbor"));
+        String bd = "SELECT Idwork, fistName, lastName, companyId, rolework FROM worker";
 
-        try {
-          Connection con = DriverManager.getConnection(url, user, pass);
-          Statement st = con.createStatement();{
-                for (User user : users) {
-                    String sql = "INSERT INTO user VALUES("
-                            + user.getId()+ "','"
-                            + user.getFistName()+ "','"
-                            + user.getLastName()+ "','"
-                            + user.getCompanyId()+ "','"
-                            + user.getRole()+ "')";
-                    st.executeUpdate(sql);
-                }
-          ResultSet res = st.executeQuery(bd);
+        try (Connection con = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/new","sanek","sanek");
+             Statement st = con.createStatement()) {
+            for (User user : users) {
+                String sql = "INSERT INTO worker VALUES("
+                        + user.getId() + ",'"
+                        + user.getFistName() + "','"
+                        + user.getLastName() + "', "
+                        + user.getCompanyId() + " ,'"
+                        + user.getRole() + "')";
+                st.executeUpdate(sql);
+            }
+            ResultSet res = st.executeQuery(bd);
 
-              while (res.next()){
-                  System.out.println("id " +res.getInt("Id"));
-                  System.out.println("name " +res.getString("fistName"));
-                  System.out.println("last name " +res.getString("lastName"));
-                  System.out.println("compani ID" +res.getInt("companId"));
-              }
-          }
-
-      } catch (SQLException e) {
-          e.printStackTrace();
-      }
+            while (res.next()) {
+                System.out.println("id " + res.getInt("Idwork")
+                        + " name " + res.getString("fistName")
+                        + " last name " + res.getString("lastName")
+                        + " company ID " + res.getInt("companyId"));
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
